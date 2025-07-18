30cba0d4c9b0fa1325722279cd6e97b0ee433583
Teja Telagathoti <telagathotiteja6522@gmail.com>
2025-07-18 19:11:11 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,26 +1,26 @@
 export const experiences = [
   {
     id: 1,
-    title: 'SWE Intern',
-    company: "ApiRSquare",
-    duration: "(Aug 2023 - Present)"
+    title: "Machine Learning Engineer",
+    company: "MetaNovaAI",
+    duration: "(Mar 2025 - Present)"
   },
   {
-    id: 2,
+    id: 3,
     title: "ROS Developer Intern",
     company: "Innovarsity",
     duration: "(Jan 2024 - Mar 2025)"
   },
   {
-    id: 3,
-    title: "ROS Intern",
-    company: "Hackboats",
-    duration: "(Feb 2024 - Apr 2024)"
+    id: 2,
+    title: 'SWE Intern',
+    company: "ApiRSquare",
+    duration: "(Aug 2023 - Mar 2025)"
   },
   {
     id: 4,
-    title: "Machine Learning Engineer",
-    company: "MetaNovaAI",
-    duration: "(Apr 2025 - Present)"
+    title: "ROS Intern",
+    company: "Hackboats",
+    duration: "(Feb 2024 - Mar 2024)"
   }
-]
+];
